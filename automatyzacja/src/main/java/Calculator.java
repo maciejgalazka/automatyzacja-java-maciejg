0f687e30043ea00ba44c15d79b702b1aeb1f147d
maciej.galazka <maciej.galazka@plus.pl>
2017-12-04 11:29:02 +0100

    dodanie zadania automatyzacja

---
+++ automatyzacja/src/main/java/Calculator.java
@@ -0,0 +1,40 @@
+public class Calculator {
+  //  private static final double POSITIVE_INFINITY=0;
+
+//  //  private int b;
+
+   // public Calculator(int a, int b) {
+  //      this.a = a;
+ //       this.b = b;
+//
+
+    public static double add (double a, double b) {
+
+        return a + b;
+    }
+
+    public static double subtract (double a, double b){
+
+        return a-b;
+    }
+
+    public static double multiply (double a, double b){
+
+        return a*b;
+    }
+
+    public static double divide (double a, double b){
+
+        if (a==0 && b==0) {
+            return 0;
+
+        }
+        else
+        return a/b;
+    }
+
+
+
+
+}
+
